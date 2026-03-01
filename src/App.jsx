bd919ad45a9e2286178b91c58d82a98ff6bccede
brandonviaje <viajebrandon@gmail.com>
2026-03-01 12:10:05 -0500

    refactor: use browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import "./styles/index.css";
-import NavBar from "./components/NavBar";
-import Hero from "./components/Hero";
+import { Routes, Route } from "react-router-dom";
 import About from "./components/About";
 import Projects from "./components/Projects";
+import Writings from "./components/Writings";
 
 function App() {
   return (
-    <div className="min-h-screen bg-[#5EFC8D] flex flex-col">
-      <section id="home"></section>
-      <NavBar />
-      <Hero />
-      <About />
-      <Projects />
+    <div className="overflow-x-hidden min-h-screen bg-[#242F40] text-white">
+      <Routes>
+        <Route index element={<About />} />
+        <Route path="projects" element={<Projects />} />
+        <Route path="writings" element={<Writings />} />
+      </Routes>
     </div>
   );
 }
