fdfad6081d109df0d755d081209fb9e6bd618a17
brandonviaje <viajebrandon@gmail.com>
2026-03-01 18:28:56 -0500

    style: change background color

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Writings from "./components/Writings";
 
 function App() {
   return (
-    <div className="overflow-x-hidden min-h-screen bg-[#242F40] text-white">
+    <div className="overflow-x-hidden min-h-screen bg-[#131316] text-white">
       <Routes>
         <Route index element={<About />} />
         <Route path="projects" element={<Projects />} />
